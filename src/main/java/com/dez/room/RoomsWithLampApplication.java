18a78f6197cddbd2621737b6c0ce38befdd932e5
denis <deniskabutkevich@gmail.com>
2020-04-29 22:18:04 +0300

    first commit

---
+++ src/main/java/com/dez/room/RoomsWithLampApplication.java
@@ -0,0 +1,13 @@
+package com.dez.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsWithLampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsWithLampApplication.class, args);
+	}
+
+}
