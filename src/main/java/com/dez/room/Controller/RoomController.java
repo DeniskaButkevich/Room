6e213c5fe3d23bb7a41815b33f9de46c4a1e3e5f
denis <deniskabutkevich@gmail.com>
2020-05-01 00:24:50 +0300

    fixed bag

--- src/main/java/com/dez/room/Controller/RoomController.java
+++ src/main/java/com/dez/room/Controller/RoomController.java
@@ -36,7 +36,7 @@ public class RoomController {
             room.setLamp(false);
         }
         roomRepo.save(room);
-        return  id ;
+        return  id + "-" + room.isLamp();
     }
 
 }
--- target/classes/static/css/app.js
+++ target/classes/static/css/app.js
@@ -33,8 +33,8 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (id) {
-            turning(id);
+        stompClient.subscribe('/topic/greetings', function (result) {
+            turning(result);
         });
     });
 }
@@ -43,24 +43,40 @@ function switching(id) {
     stompClient.send("/app/hello", {}, id);
 }
 
-function turning(id) {
-    var th_id = 'th-' + id.body;
-    var td_id = 'td-' + id.body;
-    var th_elem = document.getElementById(th_id);
-    var td_elem = document.getElementById(td_id);
+function turning(result) {
 
+    var a = result.body.split('-');
+    var str = a[1];
+
+    var th_id = 'th-' + a[0];
+    var th_elem = document.getElementById(th_id);
     if(th_elem != null){
-        if(th_elem.classList.contains("lamp-active")){
+        if(str == 'false'){
             th_elem.classList.remove("lamp-active")
         }else {
             th_elem.classList.add("lamp-active");
         }
     }
+
+    var td_id = 'td-' + a[0];
+    var td_elem = document.getElementById(td_id);
     if(td_elem != null) {
-        if (td_elem.innerText == "On") {
+        if (str == 'false') {
             td_elem.innerText = "Off";
         } else {
             td_elem.innerText = "On";
+        }
+    }
+
+    var td_id = 'body_room';
+    var td_elem = document.getElementById(td_id)
+    if(td_elem != null) {
+        if (str == 'false') {
+            td_elem.classList.remove('body-active-true')
+            td_elem.classList.add('body-active-false')
+        } else {
+            td_elem.classList.remove('body-active-false')
+            td_elem.classList.add('body-active-true')
         }
     }
 }
